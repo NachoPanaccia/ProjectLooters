50fe0b0b3bcd81fdee7f7a85078a3ab93d02f096
6uara <juanguaragnini@gmail.com>
2025-06-26 14:24:16 -0300

    last push

--- Assets/WinConditionMsg.cs
+++ Assets/WinConditionMsg.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinConditionMsg : MonoBehaviour
 {
@@ -10,6 +11,25 @@ public class WinConditionMsg : MonoBehaviour
     void Start()
     {
         my_textMesh = GetComponent<TextMeshProUGUI>();
+        switch (GameManager.instance.actualwin)
+        {
+            case wincondition.tiempo:
+                my_textMesh.text = "Time ran out, sun is shining and so are you";
+                break;
+            case wincondition.lootpermadead:
+                my_textMesh.text = "Looters Died";
+                break;
+            case wincondition.copdead:
+                my_textMesh.text = "The Cop was killed by the Looters";
+                break;
+            case wincondition.quota:
+                my_textMesh.text = "Looters reached quota";
+                break;
+            default:
+                Debug.Log("ERROR ID NO RECONOCIDO");
+                break;
+
+        }
     }
 
 
