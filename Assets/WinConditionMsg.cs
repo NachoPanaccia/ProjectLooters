52cb7feb74b514f582c66ae2f38a6282287b7649
FranPisarri <franciscopisarri@gmail.com>
2025-06-26 14:10:08 -0300

    Win scenes

---
+++ Assets/WinConditionMsg.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WinConditionMsg : MonoBehaviour
+{
+    TextMeshProUGUI my_textMesh;
+
+    void Start()
+    {
+        my_textMesh = GetComponent<TextMeshProUGUI>();
+    }
+
+
+}
